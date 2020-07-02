e4916b5df60aa53c2e9cef2e42000daecbfc5880
Rafael Fraga <ti.rafaelfraga@gmail.com>
2020-07-02 10:42:43 -0300

    Adiciona LoginActivity

---
+++ app/src/main/java/com/rafaelfraga/appdespesas/activities/LoginActivity.java
@@ -0,0 +1,15 @@
+package com.rafaelfraga.appdespesas.activities;
+
+import androidx.appcompat.app.AppCompatActivity;
+import android.os.Bundle;
+
+import com.rafaelfraga.appdespesas.R;
+
+public class LoginActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_login);
+    }
+}
