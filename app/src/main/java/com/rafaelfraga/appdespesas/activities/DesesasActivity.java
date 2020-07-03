eed53b36a8643faf07e5eb8790aa159d0221586f
Rafael Fraga <ti.rafaelfraga@gmail.com>
2020-07-03 12:52:46 -0300

    Adiciona DespesasActivity

---
+++ app/src/main/java/com/rafaelfraga/appdespesas/activities/DesesasActivity.java
@@ -0,0 +1,18 @@
+package com.rafaelfraga.appdespesas.activities;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+import com.rafaelfraga.appdespesas.R;
+
+public class DesesasActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_desesas);
+
+
+    }
+}
