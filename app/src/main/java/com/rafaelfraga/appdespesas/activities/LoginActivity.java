2a8996554d0818f7d81c5d2bf2c40ad318b08e39
Rafael Fraga <ti.rafaelfraga@gmail.com>
2020-07-02 22:21:34 -0300

    Adiciona o metodo validaCampos

--- app/src/main/java/com/rafaelfraga/appdespesas/activities/LoginActivity.java
+++ app/src/main/java/com/rafaelfraga/appdespesas/activities/LoginActivity.java
@@ -2,14 +2,57 @@ package com.rafaelfraga.appdespesas.activities;
 
 import androidx.appcompat.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.Toast;
 
+import com.google.firebase.auth.FirebaseAuth;
 import com.rafaelfraga.appdespesas.R;
+import com.rafaelfraga.appdespesas.models.Usuario;
 
 public class LoginActivity extends AppCompatActivity {
+
+    private EditText mEmail;
+    private EditText mSenha;
+    private Button mEntrar;
+    private Usuario usuario;
+    private FirebaseAuth mAuth;
+
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_login);
+
+        mEmail = findViewById(R.id.etEmail);
+        mSenha = findViewById(R.id.etSenha);
+        mEntrar = findViewById(R.id.btnEntrar);
+
+        mEntrar.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                String email = mEmail.getText().toString();
+                String senha = mSenha.getText().toString();
+
+                validaCampos(email, senha);
+
+                usuario = new Usuario();
+                usuario.setEmail(email);
+                usuario.setSenha(senha);
+
+
+            }
+        });
     }
+
+    public void validaCampos(String email, String senha) {
+        if (email.isEmpty() || senha.isEmpty()) {
+            Toast.makeText(this, "Todos os campos são obrigatórios", Toast.LENGTH_SHORT).show();
+            return;
+        }
+    }
+
+
+
 }
