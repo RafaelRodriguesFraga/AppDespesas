6e1b451831499203041731d2a95b2a7e21177c73
Rafael Fraga <ti.rafaelfraga@gmail.com>
2020-07-03 10:33:01 -0300

    Adiciona o metodo validaCampos

--- app/src/main/java/com/rafaelfraga/appdespesas/activities/LoginActivity.java
+++ app/src/main/java/com/rafaelfraga/appdespesas/activities/LoginActivity.java
@@ -1,5 +1,6 @@
 package com.rafaelfraga.appdespesas.activities;
 
+import androidx.annotation.NonNull;
 import androidx.appcompat.app.AppCompatActivity;
 import android.os.Bundle;
 import android.view.View;
@@ -7,8 +8,16 @@ import android.widget.Button;
 import android.widget.EditText;
 import android.widget.Toast;
 
+import com.google.android.gms.tasks.OnCompleteListener;
+import com.google.android.gms.tasks.Task;
+import com.google.firebase.auth.AuthResult;
 import com.google.firebase.auth.FirebaseAuth;
+import com.google.firebase.auth.FirebaseAuthInvalidCredentialsException;
+import com.google.firebase.auth.FirebaseAuthInvalidUserException;
+import com.google.firebase.auth.FirebaseAuthUserCollisionException;
+import com.google.firebase.auth.FirebaseAuthWeakPasswordException;
 import com.rafaelfraga.appdespesas.R;
+import com.rafaelfraga.appdespesas.config.FirebaseConfig;
 import com.rafaelfraga.appdespesas.models.Usuario;
 
 public class LoginActivity extends AppCompatActivity {
@@ -35,24 +44,49 @@ public class LoginActivity extends AppCompatActivity {
                 String email = mEmail.getText().toString();
                 String senha = mSenha.getText().toString();
 
-                validaCampos(email, senha);
+                if (email.isEmpty() || senha.isEmpty()) {
+                    Toast.makeText(LoginActivity.this, "Todos os " +
+                            "campos são obrigatórios", Toast.LENGTH_SHORT).show();
+                    return;
+                }
 
                 usuario = new Usuario();
                 usuario.setEmail(email);
                 usuario.setSenha(senha);
+
+                entrar();
 
 
             }
         });
     }
 
-    public void validaCampos(String email, String senha) {
-        if (email.isEmpty() || senha.isEmpty()) {
-            Toast.makeText(this, "Todos os campos são obrigatórios", Toast.LENGTH_SHORT).show();
-            return;
-        }
+    public void entrar() {
+        mAuth = FirebaseConfig.getFirebaseAuth();
+        mAuth.signInWithEmailAndPassword(usuario.getEmail(), usuario.getSenha())
+                .addOnCompleteListener(new OnCompleteListener<AuthResult>() {
+                    @Override
+                    public void onComplete(@NonNull Task<AuthResult> task) {
+                        if(task.isSuccessful()) {
+                            Toast.makeText(LoginActivity.this, "Sucesso ao fazer login",
+                                    Toast.LENGTH_SHORT).show();
+                        }else {
+                            String excecao = "";
+                            try {
+                                throw task.getException();
+                            } catch (FirebaseAuthInvalidUserException e) {
+                                excecao = "Usuário inválido ou desabilitado";
+                            } catch (FirebaseAuthInvalidCredentialsException e) {
+                                excecao = "Usuário ou senha inválidos";
+                            } catch (Exception e) {
+                                excecao = "Erro ao cadastrar o usuário: " + e.getMessage();
+                                e.printStackTrace();
+                            }
+
+                            Toast.makeText(LoginActivity.this, excecao, Toast.LENGTH_SHORT).show();
+                        }
+                    }
+                });
     }
-
-
 
 }
