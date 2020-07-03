6248c0c27efa4f849b82a5686267a59b4b558da4
Rafael Fraga <ti.rafaelfraga@gmail.com>
2020-07-03 13:06:49 -0300

    Renomeia Desesas para Despesas

--- app/src/main/java/com/rafaelfraga/appdespesas/activities/DespesasActivity.java
+++ app/src/main/java/com/rafaelfraga/appdespesas/activities/DespesasActivity.java
@@ -11,7 +11,7 @@ public class DespesasActivity extends AppCompatActivity {
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_desesas);
+        setContentView(R.layout.activity_despesas);
 
 
     }
