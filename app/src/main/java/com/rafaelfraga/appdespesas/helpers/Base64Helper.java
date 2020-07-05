1fe83e2605cd95c4122ab5927a82b3a442d80515
Rafael Fraga <ti.rafaelfraga@gmail.com>
2020-07-04 23:36:45 -0300

    Adiciona a classe Base64Helper

---
+++ app/src/main/java/com/rafaelfraga/appdespesas/helpers/Base64Helper.java
@@ -0,0 +1,14 @@
+package com.rafaelfraga.appdespesas.helpers;
+
+import android.util.Base64;
+
+public class Base64Helper {
+    public static String codificarBase64(String texto) {
+        return Base64.encodeToString(texto.getBytes(), Base64.DEFAULT)
+                .replaceAll("(\\n|\\r)", "");
+    }
+
+    public static String decodificarBase64(String textoCodificado) {
+        return new String(Base64.decode(textoCodificado, Base64.DEFAULT));
+    }
+}
