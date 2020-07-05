a37dfcf400325199f74c27001c577235541ad8b1
Rafael Fraga <ti.rafaelfraga@gmail.com>
2020-07-05 01:19:34 -0300

    Renomeia o metodo getFirebaseReference

--- app/src/main/java/com/rafaelfraga/appdespesas/config/FirebaseConfig.java
+++ app/src/main/java/com/rafaelfraga/appdespesas/config/FirebaseConfig.java
@@ -1,15 +1,26 @@
 package com.rafaelfraga.appdespesas.config;
 
 import com.google.firebase.auth.FirebaseAuth;
+import com.google.firebase.database.DatabaseReference;
+import com.google.firebase.database.FirebaseDatabase;
 
 public class FirebaseConfig {
 
     private static FirebaseAuth mAuth;
+    private static DatabaseReference mReference;
 
     public static FirebaseAuth getFirebaseAuth() {
         if(mAuth == null) {
             mAuth = FirebaseAuth.getInstance();
         }
         return mAuth;
+    }
+
+    public static DatabaseReference getFirebaseReference() {
+        if(mReference == null) {
+            mReference = FirebaseDatabase.getInstance().getReference();
+        }
+
+        return mReference;
     }
 }
