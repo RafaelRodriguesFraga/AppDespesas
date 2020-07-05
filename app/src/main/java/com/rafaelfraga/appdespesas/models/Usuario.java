5ca1cdf041cbae34d029ac6111725426be0c5969
Rafael Fraga <ti.rafaelfraga@gmail.com>
2020-07-05 01:52:23 -0300

    Adiciona campos receitaTotal e despesaTotal

--- app/src/main/java/com/rafaelfraga/appdespesas/models/Usuario.java
+++ app/src/main/java/com/rafaelfraga/appdespesas/models/Usuario.java
@@ -5,10 +5,13 @@ import com.google.firebase.database.Exclude;
 import com.rafaelfraga.appdespesas.config.FirebaseConfig;
 
 public class Usuario {
+
     private String id;
     private String nome;
     private String email;
     private String senha;
+    private Double receitaTotal = 0.00;
+    private Double despesaTotal = 0.00;
 
     public Usuario(String nome, String email, String senha) {
         this.nome = nome;
@@ -49,16 +52,32 @@ public class Usuario {
         return email;
     }
 
-    @Exclude
     public void setEmail(String email) {
         this.email = email;
     }
 
+    @Exclude
     public String getSenha() {
         return senha;
     }
 
     public void setSenha(String senha) {
         this.senha = senha;
+    }
+
+    public Double getReceitaTotal() {
+        return receitaTotal;
+    }
+
+    public void setReceitaTotal(Double receitaTotal) {
+        this.receitaTotal = receitaTotal;
+    }
+
+    public Double getDespesaTotal() {
+        return despesaTotal;
+    }
+
+    public void setDespesaTotal(Double despesaTotal) {
+        this.despesaTotal = despesaTotal;
     }
 }
