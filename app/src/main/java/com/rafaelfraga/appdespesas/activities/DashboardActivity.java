548ba54ee24e33520fdda51fcdf2c1a77ef161cc
Rafael Fraga <ti.rafaelfraga@gmail.com>
2020-07-07 23:59:57 -0300

    Adiciona logout e o metodo recuperarResumo

--- app/src/main/java/com/rafaelfraga/appdespesas/activities/DashboardActivity.java
+++ app/src/main/java/com/rafaelfraga/appdespesas/activities/DashboardActivity.java
@@ -1,27 +1,48 @@
 package com.rafaelfraga.appdespesas.activities;
 
+import androidx.annotation.NonNull;
 import androidx.appcompat.app.ActionBar;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.content.Intent;
 import android.os.Bundle;
 import android.view.View;
+import android.widget.ImageView;
 import android.widget.TextView;
 import android.widget.Toast;
 
 import com.github.clans.fab.FloatingActionButton;
+import com.google.firebase.auth.FirebaseAuth;
+import com.google.firebase.database.DataSnapshot;
+import com.google.firebase.database.DatabaseError;
+import com.google.firebase.database.DatabaseReference;
+import com.google.firebase.database.ValueEventListener;
 import com.prolificinteractive.materialcalendarview.CalendarDay;
 import com.prolificinteractive.materialcalendarview.MaterialCalendarView;
 import com.prolificinteractive.materialcalendarview.OnMonthChangedListener;
 import com.rafaelfraga.appdespesas.R;
+import com.rafaelfraga.appdespesas.config.FirebaseConfig;
+import com.rafaelfraga.appdespesas.helpers.Base64Helper;
+import com.rafaelfraga.appdespesas.models.Usuario;
 
-public class DashboardActivity extends AppCompatActivity implements View.OnClickListener{
+import java.text.DecimalFormat;
+
+public class DashboardActivity extends AppCompatActivity implements View.OnClickListener {
 
     private FloatingActionButton mDespesa;
     private FloatingActionButton mReceita;
     private MaterialCalendarView mCalendarView;
     private TextView mSaudacao;
     private TextView mSaldo;
+    private ImageView mSair;
+
+    private FirebaseAuth mAuth = FirebaseConfig.getFirebaseAuth();
+    private DatabaseReference mRef = FirebaseConfig.getFirebaseReference();
+
+    private Double mDespesaTotal = 0.00;
+    private Double mReceitaTotal = 0.00;
+    private Double mCalculoSaldo = 0.00;
+
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -33,11 +54,14 @@ public class DashboardActivity extends AppCompatActivity implements View.OnClick
         mSaudacao = findViewById(R.id.tvSaudacao);
         mSaldo = findViewById(R.id.tvSaldo);
         mCalendarView = findViewById(R.id.calendarView);
+        mSair = findViewById(R.id.ivSair);
 
         configurarCalendarView();
+        recuperarResumo();
 
         mDespesa.setOnClickListener(this);
         mReceita.setOnClickListener(this);
+        mSair.setOnClickListener(this);
     }
 
     @Override
@@ -46,23 +70,58 @@ public class DashboardActivity extends AppCompatActivity implements View.OnClick
             case R.id.fabDespesa:
                 Intent intentDespesa = new Intent(DashboardActivity.this, DespesasActivity.class);
                 startActivity(intentDespesa);
-            break;
+                break;
 
             case R.id.fabReceita:
                 Intent imtentReceita = new Intent(DashboardActivity.this, ReceitasActivity.class);
                 startActivity(imtentReceita);
+                break;
+
+            case R.id.ivSair:
+                mAuth.signOut();
+
+                Intent intentMain = new Intent(DashboardActivity.this, MainActivity.class);
+                startActivity(intentMain);
+                finish();
                 break;
         }
     }
 
     public void configurarCalendarView() {
         CharSequence meses[] = {"Janeiro", "Fevereiro", "Março", "Abril", "Maio", "Junho", "Julho",
-        "Agosto", "Setmbro", "Outubro", "Novembro", "Dezembro"};
+                "Agosto", "Setmbro", "Outubro", "Novembro", "Dezembro"};
         mCalendarView.setTitleMonths(meses);
 
         mCalendarView.setOnMonthChangedListener(new OnMonthChangedListener() {
             @Override
             public void onMonthChanged(MaterialCalendarView widget, CalendarDay date) {
+
+            }
+        });
+    }
+
+    public void recuperarResumo() {
+        String id = Base64Helper.codificarBase64(mAuth.getCurrentUser().getEmail());
+        DatabaseReference reference = mRef.child("usuarios").child(id);
+
+        reference.addValueEventListener(new ValueEventListener() {
+            @Override
+            public void onDataChange(@NonNull DataSnapshot dataSnapshot) {
+                Usuario usuario = dataSnapshot.getValue(Usuario.class);
+                mDespesaTotal = usuario.getDespesaTotal();
+                mReceitaTotal = usuario.getReceitaTotal();
+
+                mCalculoSaldo = mReceitaTotal - mDespesaTotal;
+
+                DecimalFormat decimalFormat = new DecimalFormat("#,##0.00");
+                String calculoSaldoFormatado = decimalFormat.format(mCalculoSaldo);
+
+                mSaudacao.setText("Olá, "+usuario.getNome());
+                mSaldo.setText("R$ "+calculoSaldoFormatado);
+            }
+
+            @Override
+            public void onCancelled(@NonNull DatabaseError databaseError) {
 
             }
         });
