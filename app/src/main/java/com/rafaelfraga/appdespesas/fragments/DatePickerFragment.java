743f2f643ebb05487ab0607dcc753c524076d7eb
Rafael Fraga <ti.rafaelfraga@gmail.com>
2020-07-09 23:36:03 -0300

    Adiciona DatePickerFragment

---
+++ app/src/main/java/com/rafaelfraga/appdespesas/fragments/DatePickerFragment.java
@@ -0,0 +1,24 @@
+package com.rafaelfraga.appdespesas.fragments;
+
+import android.app.DatePickerDialog;
+import android.app.Dialog;
+import android.os.Bundle;
+
+import androidx.annotation.NonNull;
+import androidx.annotation.Nullable;
+import androidx.fragment.app.DialogFragment;
+
+import java.util.Calendar;
+
+public class DatePickerFragment extends DialogFragment {
+    @NonNull
+    @Override
+    public Dialog onCreateDialog(@Nullable Bundle savedInstanceState) {
+        Calendar calendario = Calendar.getInstance();
+        int ano = calendario.get(Calendar.YEAR);
+        int mes = calendario.get(Calendar.MONTH);
+        int dia = calendario.get(Calendar.DAY_OF_MONTH);
+        return new DatePickerDialog(getActivity(), (DatePickerDialog.OnDateSetListener) getActivity(),
+                ano, mes, dia );
+    }
+}
